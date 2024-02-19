be7141f51f8c4f3b92e5b8019a25d9b9676e04d3
BhavikOstwal <bhavikostwal45@gmail.com>
2024-02-20 00:39:02 +0530

    added swaps and comparisons in sorting algo in Lab02

--- Lab_02/Q.2.cpp
+++ Lab_02/Q.2.cpp
@@ -1,57 +1,51 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void merge(int A[], int p, int q, int r)
-{
-    int n1 = q - p + 1;
-    int n2 = r - q;
-    int L[n1 + 1], R[n2 + 1];
-    for (int i = 0; i < n1; i++)
-    {
-        L[i] = A[p-1+i];
-    }
-    for (int i = 0; i < n2; i++)
-    {
-        R[i] = A[q + i];
-    }
-
-    L[n1] = 1215752191;
-    R[n2] = 1215752191;
-    int j = 0, k = 0;
-    for (int i = p-1; i < r; i++)
-    {
-        if (R[k] < L[j])
-        {
-            A[i] = R[k];
-            k++;
+int comparisons=0;
+void merge(int A[], int p, int q, int r){
+		int n1 = q-p+1;
+        int n2 = r-q;
+        int L[n1+1],R[n2+1];
+        for (int i = 0; i<n1; i++){
+                L[i]=A[p+i];
         }
-        else
-        {
-            A[i] = L[j];
-            j++;
+        for (int j = 0; j<n2; j++){
+                R[j]=A[q+1+j];
         }
-    }
+        L[n1] = 999999;
+        R[n2] = 999999;
+        int i = 0, j = 0;
+		for (int k=p; k<r+1; k++){
+			comparisons++;
+			if (L[i]<R[j]){
+				A[k]=L[i];
+				i++;
+			}
+			else {
+				A[k]=R[j];
+				j++;
+			}
+		}
 }
 
-void merge_sort(int A[], int p, int r){   
-    if(p<r){
-        int q = (p + r) / 2;
-        merge_sort(A, p, q);
-        merge_sort(A, q + 1, r);
-        merge(A, p, q, r);
-    }
+void merge_sort(int A[], int p, int r){
+	comparisons++;
+	if (p<r){
+		int q = p + (r-p)/2;	
+		merge_sort(A, p, q);
+		merge_sort(A, q+1, r);
+		merge(A, p, q, r);
+	}
 }
 
-int main()
-{
-    int arr[8] = {3, 2, 56, 89, 4, 5, 9, 1};
-    merge_sort(arr, 1, 8);
-
-    for (int i = 0; i < 8; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
+int main(){
+	int A[] = {12, 11, 13, 5, 6};
+    merge_sort(A, 0, 4);
+	
+	// printing array A[] 
+	for (int i=0; i<5; i++){
+		cout<<A[i]<<" ";
+	}
+	cout<<"\nComparisons: "<<comparisons<<endl;
     return 0;
 }
